be372c583159ae33562cd8d34daae55633ab2619
dimpho-244 <St10464062@rcconnect.edu.za>
2026-04-08 19:38:59 +0200

    Update Login.java

--- St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Login.java
+++ St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Login.java
@@ -14,10 +14,39 @@ public class Login {
     String enetredPasword;
 
     
+    //Check if username includes the right charecters and that it is no more than 5 letters
     public boolean checkUsername() {
         
         return (Registration.getUsername().contains("_")&& Registration.getUsername().length() <=5);
         
     }
     
+    //Check if pasword meets the correct rule and requirements
+   
+            public boolean checkPasswordComplexity(){
+
+
+        boolean iUpper = false ;
+        boolean isSpecialCharecter = false ;
+        boolean isNumber = false ; 
+        char currentCharecter ;
+
+            if (Regitration.getPassword().length() >=8) {
+                
+                        
+                        for (int i = 0;i < Registratio.getPasword().length(); i++) {
+                            
+                            currentCharecter = Registration.getPassword().charAt(i);
+                            if (Character.isUpperCase(currentCharecter)) {
+                                isUpper = true;
+                                else if (Character.isDigit(currentCharecter)) {
+                                        isNumber = true;
+                                        else if (!(Character.isLetterOrDigit(currentCharecter)) {
+                                         isSpecialCharecter = true;
+                              
+                                        }
+                                        }
+                            
+                        }
+            }
 }

19822b6d8a827f74eb10ec4305fec1771aa5e21d
dimpho-244 <St10464062@rcconnect.edu.za>
2026-04-08 14:37:23 +0200

    Update Registration.java

--- St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Registration.java
+++ St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Registration.java
@@ -4,11 +4,14 @@
  */
 package com.mycompany.st10464062poepart1;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 /**
  *
  * @author user
  */
 public class Registration {
+    
     static Login loginObject = new Login();
     
     static Scanner input = new Scanner(System.in);
@@ -17,4 +20,9 @@ public class Registration {
     public static String Surname    = "";  
     private static String Paswrord = "";
     private static String Username = "";
+
+    public static void setUsername(){
+        Username = JOptionPane.showInputDialog("Please enter a username that contains an underscore (_) and is no more than 5 letters");
+    }
 }
+

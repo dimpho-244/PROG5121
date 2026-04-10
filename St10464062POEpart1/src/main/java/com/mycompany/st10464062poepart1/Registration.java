755a9a518f5ba5c731fc0e4bd89ad3e8d4b6b4ed
dimpho-244 <St10464062@rcconnect.edu.za>
2026-04-10 16:02:24 +0200

    Update Registration.java

--- St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Registration.java
+++ St10464062POEpart1/src/main/java/com/mycompany/st10464062poepart1/Registration.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.st10464062poepart1;
+
 
 import java.util.Scanner;
 import javax.swing.JOptionPane;

e94180e5730b84d887118ce4b80faf0afa145451
dimpho-244 <St10464062@rcconnect.edu.za>
2026-06-12 22:29:18 +0200

    Add files via upload

---
+++ St10464062POEpart3/src/test/java/RegistrationLoginTest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author user
+ */
+public class RegistrationLoginTest {
+   @Test 
+    public void testCheckUsername_Correct() {
+        Login Login = new Login(); 
+        assertEquals(true,Login.checkUsername("kyl_1"));
+    }
+    @Test 
+    public void testCheckUsername_Incorrect() {
+        Login Login = new Login(); 
+        assertEquals(false,Login.checkUsername("“kyle!!!!!!!"));
+    }
+    
+    @Test 
+    public void testPasswordCorrect() {
+        Login Login = new Login(); 
+        assertEquals(true,Login.checkUsername("Ch&&sec@ke99!"));
+    }
+    
+    @Test 
+    public void testPassword_Incorrect() {
+        Login Login = new Login(); 
+        assertEquals(false,Login.checkUsername("password"));
+    }
+    
+    @Test 
+    public void testCellphoneCorrect() {
+        Login Login = new Login(); 
+        assertEquals(true,Login.checkCellphoneNumber("+27838968976"));
+    }
+    @Test 
+    public void testCellphone_Incorrect() {
+        Login Login = new Login(); 
+        assertEquals(false,Login.checkCellphoneNumber("08966553"));
+                
+    }
+    
+}

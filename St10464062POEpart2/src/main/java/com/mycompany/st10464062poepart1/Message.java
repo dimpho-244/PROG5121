afbb340ffebeb84168c0f9d20a1f01f2c823eedd
dimpho-244 <St10464062@rcconnect.edu.za>
2026-06-12 18:11:44 +0200

    Update Message.java

--- St10464062POEpart2/src/main/java/com/mycompany/st10464062poepart1/Message.java
+++ St10464062POEpart2/src/main/java/com/mycompany/st10464062poepart1/Message.java
@@ -54,6 +54,134 @@ public class Message {
     public int returnTotalMessages() {
         return totalMesages;
     }
+
+//Part 3 Methods 
+    public void storeMessageJSON(String id, String hash, String reciepient, String message, int option) {
+        
+        messageID.add(id);
+        reciepients.add(reciepients);
+        messageHashes.add(hash);
+        
+        if (option == 1) {
+            sentMessages.add(message);
+        } else if (option == 2) {
+            disregardedMessages.add(message);
+        } else if (option == 3) {
+            storedMessages.add(message);
+        }
+    }
+    public ArrayList<String> getSentMessages() {
+        return sentMessages;
+    }
+    public ArrayList<String> getStoredMessages() {
+        return storedMessages;
+    }
+    public ArrayList<String> getDisregardedMessages() {
+        return disregardedMessages;
+    }
+    
+    //Display sender and recipient of sent mesages
+    public String displaySentMessages() {
+        
+        String Output = "";
+        
+        for (int i = 0; i < sentMessages.size(); i++) {
+            
+            output += "Sender: Developer\n";
+            output += "Recipient: + recipients.get(i) + "\\n\\n\";
+        }
+        return output;
+    }
+    //Longest message
+    public String getLongestMessage() {
+        
+        String longest = "";
+        
+        for (String msg : sentMessages) {
+            
+            if (msg.length() > longest.length()) {
+                longest = msg;
+            }
+        }
+        for (String msg : storedMessages) {
+            
+            if (msg.length() > longest.length()) {
+        }
+    }
+        return longest;
+    }
+    
+    //Searching by Message ID
+    public String searchMessageID(String id) {
+        
+        for (int i = 0; i < messageIDs.size(); i++) {
+            if (messagesIDs.get(i).equals(id)) {
+                
+                if (i < sentMessages.size()) {
+                    return sentMessages.get(i);
+                }
+            }
+        }
+        return "Message not found.";
+    }
+    
+    //Search by recipient 
+    public String deleteMessage(String hash) {
+        
+        String result = ""; 
+        
+        for (int i = 0; i  < reciepients.size(); i++) {
+            if (reciepients.get(i).equals(reciepients)) {
+                
+                if(i < storedMessages.size()) {
+                    result += storedMessages.get(i) + "\n";
+                }
+            }hhjgfc
+        }
+        return result;
+    }
+    //Delete by Hash
+    public String deleteMessage(String hash) {
+        
+        for (int i = 0; i < messageHashes.size(); i++) {
+            if (messageHashes.get(i).equals(hash)) {
+                String deletedMessage = "";
+                
+                if (i < storedMessages.size()) {
+                    
+                    deletedMessage = storedMessages.get(i);
+                    storedMessages.remove(i);
+                }
+                messageHashes.remove(i);
+                return "Message \"" + deletedMessage + "\" sucesfully deleted.";
+            }
+        }
+        return "Message not found.";
+    }
+    
+   //Display report
+    public String displayReport() {
+         
+        String report = "MESSAGE REPORT\n";
+        report += "============================\n\n";
+        
+        for (int i = 0; i < sentMessages.size(); i++) {
+            
+            report += "Message Hash: "
+                    + messageHashes.get(i)
+                    + "\n";
+            
+            report += "Recipient: "
+                    + reciepients.get(i)
+                    + "\n";
+            
+            report += "Message: "
+                    + sentMessages.get(i)
+                    + "\n";
+            
+             }
+        return report;
+    }
     //Storing of the message in JSON
     public void storeMessageJSON(String id, String hash, String reciepient, String message) {
         
